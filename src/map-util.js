596fd83e1187431a4f629d52af9ee092d9ea176f
John Ovalle <johnovalle@gmail.com>
2017-11-17 08:17:45 -0500

    remove magic numbers from map-utils, need test against other map sizes

--- src/map-util.js
+++ src/map-util.js
@@ -1,35 +1,37 @@
+//bring these in from config
 let mapWidth = 27;
 let mapHeight = 27;
+let tileSize = 32;
 const rowsToShow = 4;
 const maxOffsetX = mapWidth - 1 - rowsToShow;
 const maxOffsetY = mapHeight - 1 - rowsToShow;
 
 
 export const indexToXY = (index) => {
-  let x = index % 27;
-  let y = Math.floor(index / 27);
+  let x = index % mapWidth;
+  let y = Math.floor(index / mapWidth);
   return {x, y};
 };
 
 export const indexTrueToXY = (index) => {
-  let x = (index % 27) * 32;
-  let y = Math.floor(index / 27) * 32;
+  let x = (index % mapWidth) * tileSize;
+  let y = Math.floor(index / mapWidth) * tileSize;
   return {x, y};
 };
 
 export const xyToIndex = (coords) => {
-  return (coords.y*27) + coords.x;
+  return (coords.y*mapWidth) + coords.x;
 };
 
 export const getTranslation = (coords) => {
   let offsetCoords = {x:0, y:0};
   let extra = 0;
-  if(coords.x > 22){
-    extra = coords.x - 22;
+  if(coords.x > maxOffsetX){
+    extra = coords.x - maxOffsetX;
   }
 
-  if(coords.x >= 4 ){
-    offsetCoords.x = coords.x - (4 + extra);
+  if(coords.x >= rowsToShow ){
+    offsetCoords.x = coords.x - (rowsToShow + extra);
   }else{
     offsetCoords.x = 0;
   }
@@ -37,8 +39,8 @@ export const getTranslation = (coords) => {
   if(coords.y > maxOffsetY){
     extra = coords.y - maxOffsetY;
   }
-  if(coords.y >= 4){
-    offsetCoords.y = coords.y - (4 + extra);
+  if(coords.y >= rowsToShow){
+    offsetCoords.y = coords.y - (rowsToShow + extra);
   }else{
     offsetCoords.y = 0;
   }
@@ -47,20 +49,20 @@ export const getTranslation = (coords) => {
 // just to get this to work, this all needs to be completely rewritten
 export const constrainCameraTranslation = (player) => {
   let coords = {x: 0, y: 0};
-  if(player.x < 128){
+  if(player.x < rowsToShow * tileSize){
     coords.x = 0;
-  }else if (player.x > 704) {
-    coords.x = -576; //-(22 - 4) * 32;
+  }else if (player.x > maxOffsetX * tileSize) {
+    coords.x = -(maxOffsetX - rowsToShow) * tileSize//-576; //-(22 - 4) * 32;
   } else {
-    coords.x = -(player.x - 128);
+    coords.x = -(player.x - (rowsToShow * tileSize));
   }
 
-  if(player.y < 128){
+  if(player.y < rowsToShow * tileSize){
     coords.y = 0;
-  }else if(player.y > 704) {
-    coords.y = -576; //-(22 - 4) * 32;
+  }else if(player.y > maxOffsetY * tileSize) {
+    coords.y = -(maxOffsetY - rowsToShow) * tileSize//-576; //-(22 - 4) * 32;
   } else {
-    coords.y = -(player.y - 128);
+    coords.y = -(player.y - (rowsToShow * tileSize));
   }
   return coords;
 };
@@ -72,20 +74,20 @@ export const moveEntity = (entity, key) => {
   entity.nextY = entity.y;
   entity.nextX = entity.x;
   if(key === "ArrowUp" && currentCoords.y > 0){
-    entity.index -= 27;
-    entity.nextY -= 32;
+    entity.index -= mapWidth;
+    entity.nextY -= tileSize;
   }
   if(key === "ArrowDown" && currentCoords.y < mapHeight - 1){
-    entity.index += 27;
-    entity.nextY += 32;
+    entity.index += mapWidth;
+    entity.nextY += tileSize;
   }
   if(key === "ArrowLeft" && currentCoords.x > 0){
     entity.index -= 1;
-    entity.nextX -= 32;
+    entity.nextX -= tileSize;
   }
-  if(key === "ArrowRight" && currentCoords.x < 26){
+  if(key === "ArrowRight" && currentCoords.x < mapWidth - 1){
     entity.index += 1;
-    entity.nextX += 32;
+    entity.nextX += tileSize;
   }
   console.log(entity);
 
