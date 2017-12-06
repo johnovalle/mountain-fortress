7fad35249b5cd6f2ddc88b12c7f21b77b83d3419
John Ovalle <johnovalle@gmail.com>
2017-12-05 23:56:54 -0500

    add fog of war, not animated which needs to be fixed

--- src/draw.js
+++ src/draw.js
@@ -13,11 +13,18 @@ export const draw = (state) => {
   ctx.clearRect(0, 0, Config.canvasWidth, Config.canvasHeight);
   ctx.save();
   if(state.currentScene.currentLevel && state.currentScene.currentLevel.map) { //Temporary
+    let currentCoords = MapUtil.indexToXY(Model.state.player.index);
+    let sightPoints = MapUtil.getAllPoints(currentCoords, 4);
+    let sightIndices = sightPoints.map((p) => {
+      return MapUtil.xyToIndex(p);
+    });
+    sightIndices.push(Model.state.player.index);
     //console.log(translateOffset);
     //ctx.translate(translateOffset.x * -spritesheet.tileSize, translateOffset.y * -spritesheet.tileSize);
     ctx.translate(translateOffset.x, translateOffset.y);
     drawMap(state.currentScene.currentLevel.map);
-    drawEntities(state.currentScene.currentLevel);
+    drawEntities(state.currentScene.currentLevel, sightIndices);
+    drawFog(state.currentScene.currentLevel.map, sightIndices);
   }
   ctx.restore();
 }
@@ -33,7 +40,7 @@ const setCameraOffset = () => {
 }
 
 //TODO DRY up these two methods
-const drawEntities = (level) => { //Temporary
+const drawEntities = (level, sightIndices) => { //Temporary
   //buildEntityMap(level);
   //console.log("entitiesMap", level.entitiesMap);
   // drawMap(level.entitiesMap);
@@ -41,16 +48,18 @@ const drawEntities = (level) => { //Temporary
   // probably also need to store the offset as X and Y so the screen shift will also be smooth
   for(let i = 0; i <  level.entities.length; i++){
     let entity = level.entities[i];
+    if (sightIndices.indexOf(entity.index) !== -1) {
     //console.log(entity.x, entity.y);
     // these properties can be stored on the entity itself rather than be calculated everytime
-    let sx = (entity.key % spritesheet.sheetCols) * spritesheet.tileSize;
-    let sy = Math.floor(entity.key / spritesheet.sheetCols) * spritesheet.tileSize;
-    ctx.drawImage(spritesheet.sheet, sx, sy, spritesheet.tileSize, spritesheet.tileSize,
-                                    entity.x, entity.y, Config.tileSize, Config.tileSize);
+      let sx = (entity.key % spritesheet.sheetCols) * spritesheet.tileSize;
+      let sy = Math.floor(entity.key / spritesheet.sheetCols) * spritesheet.tileSize;
+      ctx.drawImage(spritesheet.sheet, sx, sy, spritesheet.tileSize, spritesheet.tileSize,
+                                      entity.x, entity.y, Config.tileSize, Config.tileSize);
+    }
   }
 }
 
-const drawMap = (map) => {
+const drawMap = (map) => { //check viewport here and only draw what's in the viewport
   for(let i = 0, len = map.grid.length;  i < len; i++){
     let tile = map.grid[i];
     if(tile !== 0 || map.isBG){
@@ -63,6 +72,16 @@ const drawMap = (map) => {
     }
   }
 };
+const drawFog = (map, sightIndices) => {
+  for(let i = 0, len = map.grid.length;  i < len; i++){
+    if (sightIndices.indexOf(i) === -1) {
+      let x = (i % map.mapCols) * Config.tileSize; // index / width of drawing area in tiles * tile size
+      let y = Math.floor(i / map.mapCols) * Config.tileSize;
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
+      ctx.fillRect(x, y, Config.tileSize, Config.tileSize);
+    }
+  }
+}
 
 // Let's see where this goes...
 const drawer = {
